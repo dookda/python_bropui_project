982490ee0226ce005e838ed8ece1231d5193789d
dookda <sakda.homhuan@gmail.com>
2021-09-07 11:53:30 +0700

    load csv to db

--- conn_ftp_read_csv.py
+++ conn_ftp_read_csv.py
@@ -1,9 +1,10 @@
-# import lib
+import os
 import pandas as pd
 import numpy as np
 import ftplib
 import csv
 import psycopg2 as pg2
+import re
 
 conn = pg2.connect(user="postgres", password="Pgis@rti2dss@2020",
                    host="119.59.125.134", database="envidb")
@@ -25,6 +26,15 @@ for r in rows:
 print(f"savedfile {savedfile}")
 
 
+def convNumber(x):
+    x = x.replace("<", "")
+    x = x.replace(">", "")
+    isNumber = any(map(str.isdigit, x))
+    z = isNumber and x or 0
+    # print(z)
+    return z
+
+
 def insertData(fpath, fname):
     dat = {}
     with open(fpath) as fd:
@@ -33,8 +43,11 @@ def insertData(fpath, fname):
 
         for r in rows[:58]:
             x = r[1].split(" ")
+            # isNumber = any(map(str.isdigit, x[0]))
+            # print(type(x[0]), x[0])
+            # xNumber = isNumber and x[0] or 0
             dat[r[0].lower()] = x[0]
-
+    # print(dat)
     sql = f'''INSERT INTO vibrate(
             ts, filename, 
             tranppv,
@@ -48,15 +61,15 @@ def insertData(fpath, fname):
             longppvdb
         )VALUES(
             '{dat["eventdate"]} {dat["eventtime"]}', '{fname}', 
-            {dat["tranppv"].replace("<","")},
-            {dat["vertppv"].replace("<","")},
-            {dat["longppv"].replace("<","")},
-            {dat["tranppvponderated"]},
-            {dat["vertppvponderated"]},
-            {dat["longppvponderated"]},
-            {dat["tranppvdb"]},
-            {dat["vertppvdb"]},
-            {dat["longppvdb"]}
+            {convNumber(dat["tranppv"])},
+            {convNumber(dat["vertppv"])},
+            {convNumber(dat["longppv"])},
+            {convNumber(dat["tranppvponderated"])},
+            {convNumber(dat["vertppvponderated"])},
+            {convNumber(dat["longppvponderated"])},
+            {convNumber(dat["tranppvdb"])},
+            {convNumber(dat["vertppvdb"])},
+            {convNumber(dat["longppvdb"])}
         )'''
     # print(sql)
 
@@ -69,16 +82,18 @@ def loadCSV(fname):
     ftp.retrbinary("RETR "+fname, open(fpath, 'wb').write)
     print(fname)
     insertData(fpath, fname)
+    os.remove(fpath)
 
 
 # retrieve file
-def initLoop():
+def initLoadLastfile():
     a = len(files)
     f = files[int(a-1)]
+    # print(f)
     loadCSV(f)
 
 
-def initLoop2():
+def initLoad():
     for f in files:
         if(f not in savedfile):
             loadCSV(f)
@@ -88,4 +103,4 @@ def initLoop2():
             print(f)
 
 
-initLoop2()
+initLoad()
